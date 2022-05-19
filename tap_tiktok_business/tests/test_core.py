86118cc530ce83fd47b18883e1fa577b2b7be5bd
Hunter Kuffel <hunter.kuffel@voxmedia.com>
2022-05-19 16:33:17 -0700

    trying to make lint happy

--- tap_tiktok_business/tests/test_core.py
+++ tap_tiktok_business/tests/test_core.py
@@ -1,24 +1,32 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
 import datetime
+import os
 
+from singer_sdk.helpers._util import read_json_file
 from singer_sdk.testing import get_standard_tap_tests
 
 from tap_tiktok_business.tap import TapTiktokBusiness
 
-SAMPLE_CONFIG = {
-    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d")
-    # TODO: Initialize minimal tap config
-}
+CONFIG_PATH = ".secrets/config.json"
+
+if os.getenv("CI"):  # true when running a GitHub Actions workflow
+    SAMPLE_CONFIG = {
+        "client_id": os.getenv("TAP_TIKTOK_CLIENT_ID"),
+        "client_secret": os.getenv("TAP_TIKTOK_CLIENT_SECRET"),
+        "access_token": os.getenv("TAP_TIKTOK_ACCESS_TOKEN"),
+        "refresh_token": os.getenv("TAP_TIKTOK_REFRESH_TOKEN"),
+        "business_ids": os.getenv("TAP_INSTAGRAM_USER_ID").split(', '),
+        "start_date": datetime.datetime.now() - datetime.timedelta(months=1)
+    }
+else:
+    SAMPLE_CONFIG = read_json_file(CONFIG_PATH)
 
 
 # Run standard built-in tap tests from the SDK:
 def test_standard_tap_tests():
     """Run standard tap tests from the SDK."""
-    tests = get_standard_tap_tests(
-        TapTiktokBusiness,
-        config=SAMPLE_CONFIG
-    )
+    tests = get_standard_tap_tests(TapTiktokBusiness, config=SAMPLE_CONFIG)
     for test in tests:
         test()
 
